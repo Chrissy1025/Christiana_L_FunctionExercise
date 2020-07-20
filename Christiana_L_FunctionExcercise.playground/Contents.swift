5b5f56dc45056b89e0f9b33ebbc673321ae76fcc
Chrissy1025 <67969110+Chrissy1025@users.noreply.github.com>
2020-07-20 10:59:15 -0500

    Add files via upload

---
+++ Christiana_L_FunctionExcercise.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Funtion Exercise"
+
+var a = 9
+var b = 19
+
+func plus(min: Int, max: Int) -> Int {
+    let a = 9
+    let b = 19
+    return a + b
+}
+
+print(plus(min: a, max: b))
+
+
+
